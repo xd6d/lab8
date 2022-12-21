965cf5a2435001d72156642499bf2bdf5934d68a
xd6d <Kurkin566@gmail.com>
2022-12-21 15:58:15 +0200

    minor updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,47 @@
 <template>
   <form class="d-flex justify-content-center align-items-center">
-    <label class="flex-row h6 m-0">Розклад для </label>
-    <v-select :options="groups" style="margin-left: 5px; min-width: 114px;" :label="'name'"/>
+    <label class="flex-row h6 m-0" for="select">Розклад для </label>
+    <v-select :options="groups" style="margin-left: 5px; min-width: 114px;" :label="'name'"
+              v-model="this.group"
+              id="select"/>
   </form>
+<!--  <Table link="departments" :headers="['Назва', 'Скорочена назва', 'Факультет']"-->
+<!--         :properties="[{name:'name', type:'text'},-->
+<!--                       {name:'shortName', type:'text'},-->
+<!--                       {name:'faculty', type:'select', selectOptions: {value: faculties, label: 'shortName'}}]"-->
+<!--         :label="(o) => o.shortName"/>-->
 </template>
 
 <script>
 import vSelect from 'vue-select';
 import 'vue-select/dist/vue-select.css'
 import {getAll} from "../services/httpService";
+import Table from "./Table.vue";
+
 export default {
   name: "Home",
-  components: {vSelect},
+  components: {Table, vSelect},
   data: () => ({
-    groups: ''
+    group: '',
+    groups: '',
+    schedules: ''
   }),
+  watch: {
+    group(){
+
+    }
+  },
   methods: {
     getGroups() {
       return getAll('groups', 0, 100, 'ASC', 'name');
+    },
+    getSchedules() {
+      return getAll('schedules', 0, 100, 'ASC', 'name');
     }
   },
   async created() {
     this.groups = (await this.getGroups()).content;
+    this.schedules = (await this.getSchedules()).content;
   }
 }
 </script>
